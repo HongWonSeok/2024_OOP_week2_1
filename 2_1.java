423df05faab63e8b7a00ff9cab4f5822c9b97769
HongWonSeok <85995027+HongWonSeok@users.noreply.github.com>
2024-03-04 12:36:39 +0900

    Add files via upload

--- 2_1.java
+++ 2_1.java
@@ -1,1 +1,10 @@
-ang 
+class test {
+  public static void main(String[] args) {
+    java.util.Scanner input = new java.util.Scanner(System.in);
+  
+    System.out.print("Enter a miles: ");
+    double miles = input.nextDouble();
+    double kilometers = miles * 1.6;
+    System.out.printf("%.1f miles is %.1f kilometers", miles, kilometers);
+  }
+}
